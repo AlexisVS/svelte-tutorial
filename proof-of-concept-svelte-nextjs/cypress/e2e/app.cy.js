e38854153286b4f3eb1cea017efe13ed340faac1
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2022-09-27 12:16:06 +0200

    update

--- proof-of-concept-svelte-nextjs/cypress/e2e/app.cy.js
+++ proof-of-concept-svelte-nextjs/cypress/e2e/app.cy.js
@@ -1,16 +1,13 @@
-const setUp = () => {
-    cy.visit('http://localhost:3000/');
-    cy.contains('Pokedex').click();
-}
-
-describe('Test the application', () => {
+describe('Test the application page', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/');
+        cy.contains('Pokedex').click();
+    })
     it('should enter in pokedex app', function () {
-        setUp()
         cy.url().should('equal', 'http://localhost:3000/pokedex')
     });
 
     it('should show pokemon show view', function () {
-        setUp()
         cy.contains('charmander').click();
         cy.url().should('include', '/pokedex/4');
     });
